0fb12c4cc9b635665933664b2cd9ccf66978273a
Chris Fields <cjfields@illinois.edu>
2020-05-27 21:36:30 -0500

    update setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/02_Genome_Assembly
+cp /home/classroom/hpcbio/mayo_workshop/02_Genome_Assembly/src/*.sh ~/02_Genome_Assembly
+cp /home/classroom/hpcbio/mayo_workshop/02_Genome_Assembly/src/assemblathon_stats.pl ~/02_Genome_Assembly
+cp /home/classroom/hpcbio/mayo_workshop/02_Genome_Assembly/data/*.fastq.gz ~/02_Genome_Assembly

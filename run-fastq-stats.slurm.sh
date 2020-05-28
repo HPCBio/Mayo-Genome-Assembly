45e50d39472ca8147a139183158e8873d371ba49
Chris Fields <cjfields@illinois.edu>
2020-05-27 22:01:48 -0500

    short-term fix in case seqkit isn't installed by CNRG

--- run-fastq-stats.slurm.sh
+++ run-fastq-stats.slurm.sh
@@ -2,11 +2,12 @@
 #SBATCH -n 1
 #SBATCH --mem=4000
 #SBATCH -p classroom
-#SBATCH --mail-type=END,FAIL
 #SBATCH -J fastq-stats 
 
+export MODULEPATH=/home/classroom/hpcbio/mayo_workshop/02_Genome_Assembly/module/:$MODULEPATH
+
 ### Load Modules
-module load seqkit/0.12.0
+module load seqkit-0.12 
 
 ### Run app on file
 seqkit stats dataset*.fastq.gz > stats.txt 
